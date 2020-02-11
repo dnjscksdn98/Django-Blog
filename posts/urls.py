73631a075ae6f5bcd8168a5d99eb58db0e34e5b5
Alex <dnjscksdn98@gmail.com>
2020-02-11 21:09:27 +0900

    Refactor using class based views

--- posts/urls.py
+++ posts/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
 
-from .views import index, blog, search, post, post_create, post_update, post_delete
+from .views import IndexView, SearchView, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from marketing.views import email_list_signup
 
 
 urlpatterns = [
-    path('', index),
-    path('blog/', blog, name='post-list'),
-    path('search/', search, name='search'),
-    path('create/', post_create, name='post-create'),
-    path('post/<id>/', post, name='post-detail'),
-    path('post/<id>/update/', post_update, name='post-update'),
-    path('post/<id>/delete/', post_delete, name='post-delete'),
-
+    path('', IndexView.as_view(), name='home'),
+    path('blog/', PostListView.as_view(), name='post-list'),
+    path('search/', SearchView.as_view(), name='search'),
+    path('create/', PostCreateView.as_view(), name='post-create'),
+    path('post/<pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('email_list_signup/', email_list_signup, name='email-list-signup')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count, Q
-from django.views import View
+from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Post, Author, PostView
 from .forms import CommentForm, PostForm
@@ -16,6 +16,35 @@ def get_author(user):
     return None
 
 
+def get_category_count():
+    # only returns the category object
+    queryset = Post.objects.values(
+        'category__title').annotate(Count('category__title'))
+    return queryset
+
+
+class IndexView(View):
+    form = EmailSignupForm()
+
+    def get(self, request, *args, **kwargs):
+        featured = Post.objects.filter(featured=True)
+        latest = Post.objects.order_by('-timestamp')[0:3]
+        context = {
+            'object_list': featured,
+            'latest': latest,
+            'form': self.form
+        }
+        return render(self.request, 'index.html', context)
+
+    def post(self, request, *args, **kwargs):
+        email = self.request.POST.get('email')
+        new_signup = Signup(email=email)
+        new_signup.save()
+        messages.info(self.request, 'Successfully subscribed.')
+        return redirect('home')
+
+
+'''
 def index(request):
     featured = Post.objects.filter(featured=True)
     latest = Post.objects.order_by('-timestamp')[0:3]
@@ -32,15 +61,25 @@ def index(request):
         'form': form
     }
     return render(request, 'index.html', context)
+'''
 
 
-def get_category_count():
-    # only returns the category object
-    queryset = Post.objects.values(
-        'category__title').annotate(Count('category__title'))
-    return queryset
+class SearchView(View):
+    def get(self, request, *args, **kwargs):
+        queryset = Post.objects.all()
+        query = request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) |
+                Q(overview__icontains=query)
+            ).distinct()
+        context = {
+            'queryset': queryset
+        }
+        return render(request, 'search_result.html', context)
 
 
+'''
 def search(request):
     queryset = Post.objects.all()
     query = request.GET.get('q')
@@ -55,9 +94,30 @@ def search(request):
         'queryset': queryset
     }
     return render(request, 'search_result.html', context)
+'''
 
 
+class PostListView(ListView):
+    form = EmailSignupForm()
+    model = Post
+    template_name = 'blog.html'
+    context_object_name = 'queryset'
+    paginate_by = 4
+
+    def get_context_data(self, **kwargs):
+        category_count = get_category_count()
+        most_recent = Post.objects.order_by('-timestamp')[0:3]
+        context = super().get_context_data(**kwargs)
+        context['category_count'] = category_count
+        context['most_recent'] = most_recent
+        context['page_request_var'] = 'page'
+        context['form'] = self.form
+        return context
+
+
+'''
 def blog(request):
+    form = EmailSignupForm()
     category_count = get_category_count()
     most_recent = Post.objects.order_by('-timestamp')[0:3]
     post_list = Post.objects.all()
@@ -82,11 +142,47 @@ def blog(request):
         'queryset': paginated_queryset,
         'most_recent': most_recent,
         'page_request_var': page_request_var,
-        'category_count': category_count
+        'category_count': category_count,
+        'form': form
     }
     return render(request, 'blog.html', context)
+'''
 
 
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+    form = CommentForm()
+
+    def get_object(self):
+        obj = super().get_object()
+        if self.request.user.is_authenticated:
+            PostView.objects.get_or_create(user=self.request.user, post=obj)
+        return obj
+
+    def get_context_data(self, **kwargs):
+        category_count = get_category_count()
+        most_recent = Post.objects.order_by('-timestamp')[0:3]
+        context = super().get_context_data(**kwargs)
+        context['most_recent'] = most_recent
+        context['category_count'] = category_count
+        context['form'] = self.form
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            post = self.get_object()
+            form.instance.user = self.request.user
+            form.instance.post = post
+            form.save()
+            return redirect(reverse('post-detail', kwargs={
+                'pk': post.pk
+            }))
+
+
+'''
 def post(request, id):
     post = get_object_or_404(Post, id=id)
     most_recent = Post.objects.order_by('-timestamp')[0:3]
@@ -112,8 +208,28 @@ def post(request, id):
         'category_count': category_count
     }
     return render(request, 'post.html', context)
+'''
 
 
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    form_class = PostForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['method'] = 'Create'
+        return context
+
+    def form_valid(self, form):
+        form.instance.author = get_author(self.request.user)
+        form.save()
+        return redirect(reverse('post-detail', kwargs={
+            'pk': form.instance.pk
+        }))
+
+
+'''
 def post_create(request):
     method = 'Create'
     form = PostForm(request.POST or None, request.FILES or None)
@@ -130,8 +246,28 @@ def post_create(request):
         'form': form
     }
     return render(request, 'post_create.html', context)
+'''
 
 
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_create.html'
+    form_class = PostForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['method'] = 'Update'
+        return context
+
+    def form_valid(self, form):
+        form.instance.author = get_author(self.request.user)
+        form.save()
+        return redirect(reverse('post-detail', kwargs={
+            'pk': form.instance.pk
+        }))
+
+
+'''
 def post_update(request, id):
     method = 'Update'
     post = get_object_or_404(Post, id=id)
@@ -153,9 +289,18 @@ def post_update(request, id):
         'form': form
     }
     return render(request, 'post_create.html', context)
+'''
 
 
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/blog'
+    template_name = 'post_confirm_delete.html'
+
+
+'''
 def post_delete(request, id):
     post = get_object_or_404(Post, id=id)
     post.delete()
     return redirect(reverse('post-list'))
+'''
